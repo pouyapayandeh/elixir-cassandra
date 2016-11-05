a57272f6dd836fe4b3cd99032f5c6a2da6850b5a
Hassan Zamani <hsn.zamani@gmail.com>
2016-11-05 20:19:06 +0330

    Add Cluster control connection down test

--- test/cassandra/cluster_test.exs
+++ test/cassandra/cluster_test.exs
@@ -2,11 +2,11 @@ defmodule Cassandra.ClusterTest do
   use ExUnit.Case
 
   alias Cassandra.Cluster
-
-  @moduletag capture_log: true
+  import ExUnit.CaptureLog
 
   @host Cassandra.TestHelper.host
 
+  @tag :capture_log
   test "no_avaliable_contact_points" do
     assert {:error, :no_avaliable_contact_points} = Cluster.start(["127.0.0.1"], [port: 9111])
   end
@@ -15,5 +15,15 @@ defmodule Cassandra.ClusterTest do
     assert {:ok, cluster} = Cluster.start_link([@host])
     hosts = Cluster.hosts(cluster)
     assert [%Cassandra.Host{status: :up} | _] = Map.values(hosts)
+  end
+
+  test "connection down" do
+    assert {:ok, cluster} = Cluster.start_link([@host])
+    conn = :sys.get_state(cluster)[:control_connection]
+    kill = fn ->
+      assert capture_log(fn -> GenServer.stop(conn, :error) end) =~ "control connection lost"
+    end
+    assert capture_log(kill) =~ "new control connection opened"
+    assert conn != :sys.get_state(cluster)[:control_connection]
   end
 end
