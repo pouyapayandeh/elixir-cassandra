532dba53d92d2476a749d653ece80da31342bc6c
Hassan Zamani <hsn.zamani@gmail.com>
2016-11-15 12:05:09 +0330

    Merge operation in frame

--- lib/cql/frame.ex
+++ lib/cql/frame.ex
@@ -3,7 +3,6 @@ defmodule CQL.Frame do
 
   import CQL.DataTypes.Encoder
 
-  alias CQL.Opration
 
   defstruct [
     version: 0x04,
@@ -14,12 +13,33 @@ defmodule CQL.Frame do
     body: "",
   ]
 
-  def encode(f = %__MODULE__{}) do
+  @operations %{
+    :ERROR          => 0x00,
+    :STARTUP        => 0x01,
+    :READY          => 0x02,
+    :AUTHENTICATE   => 0x03,
+    :OPTIONS        => 0x05,
+    :SUPPORTED      => 0x06,
+    :QUERY          => 0x07,
+    :RESULT         => 0x08,
+    :PREPARE        => 0x09,
+    :EXECUTE        => 0x0A,
+    :REGISTER       => 0x0B,
+    :EVENT          => 0x0C,
+    :BATCH          => 0x0D,
+    :AUTH_CHALLENGE => 0x0E,
+    :AUTH_RESPONSE  => 0x0F,
+    :AUTH_SUCCESS   => 0x10,
+  }
+
+  @operation_names @operations |> Enum.map(fn {k, v} -> {v, k} end) |> Enum.into(%{})
+
+  def encode(%__MODULE__{} = f) do
     Enum.join [
       byte(f.version),
       byte(f.flags),
       short(f.stream),
-      Opration.encode(f.opration),
+      byte(Map.fetch!(@operations, f.opration)),
       int(byte_size(f.body)),
       f.body,
     ]
@@ -39,7 +59,7 @@ defmodule CQL.Frame do
       version: version,
       flags: flags,
       stream: stream,
-      opration: Opration.decode(opcode),
+      opration: Map.fetch!(@operation_names, opcode),
       length: length,
       body: body,
     }
